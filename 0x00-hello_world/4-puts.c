306b1b2fa15503f978662ebc9eb0dc30ab1d1267
StaciAF <aaen.it19@gmail.com>
2020-02-05 23:29:24 +0000

    adds C program that prints string followed by new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void);
+{
+/* prints string */
+int puts("Programming is like building a multilingual puzzle");
+int getchar(void);
+/* return 0 after prints string */
+return (0);
+}
