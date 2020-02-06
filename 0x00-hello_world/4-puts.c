93f3c844a02dacda449dd686e91c7ad9a43219fd
StaciAF <aaen.it19@gmail.com>
2020-02-06 00:06:22 +0000

    adds C program that prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/* main entry point*/
 int main(void)
+/* main entry point */
 {
 /* prints string on screen */
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 /* return 0 after prints string */
 return (0);
 }
