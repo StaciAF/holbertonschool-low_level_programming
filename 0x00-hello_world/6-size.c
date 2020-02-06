3df193cb4f5511676e4fbe3ce8c2c33aa3c132c8
StaciAF <aaen.it19@gmail.com>
2020-02-06 01:44:42 +0000

    adds C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/**                                                                                      
+ * main - Entry point                                                                    
+ *                                                                                       
+ * Return: Always 0 (Success)                                                            
  */
 int main(void)
 {
-char char1;
-int int1;
-int int2;
-long long int longlongint1;
-float float1;
+  char char1;
+  int int1;
+  int int2;
+  long long int longlongint1;
+  float float1;
 
-printf("Size of a char: %ld bytes(s) \n", sizeof(char1));
-printf("Size of an int: %ld byte(s)\n", sizeof(int1));
-printf("Size of a long int: %ld byte(s)\n", sizeof(int2));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongint1));
-printf("Size of a float: %ld byte(s)\n", sizeof(float1));
-return (0);
+  printf("Size of a char: %ld bytes(s) \n", sizeof(char1));
+  printf("Size of an int: %ld byte(s)\n", sizeof(int1));
+  printf("Size of a long int: %ld byte(s)\n", sizeof(int2));
+  printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongint1));
+  printf("Size of a float: %ld byte(s)\n", sizeof(float1));
+  return (0);
 }
