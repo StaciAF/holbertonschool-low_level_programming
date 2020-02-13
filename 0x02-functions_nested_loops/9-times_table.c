82bb76d3e7d00486e0847bd81b9b02eea0d54cbd
StaciAF <aaen.it19@gmail.com>
2020-02-13 03:30:27 +0000

    adds function to calculate multiples of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+* times_table - prints the 9 times table
+*@side: takes column input
+*
+*@top: takes row input
+*
+*@x: takes int
+*
+* Description - starts with 0 prints 9 times table
+* Return: in main function
+*/
+
+
+void times_table(void)
+{
+	int side, top, x;
+
+for (side = 0; side <= 9; side++)
+{
+	_putchar('0');
+
+	for (top = 1; top <= 9; top++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		x = top * side;
+	if ((x / 10) == 0)
+	{
+		_putchar(' ');
+		_putchar(x + '0');
+	}
+	else
+	{
+		_putchar((x / 10) + '0');
+		_putchar((x % 10) + '0');
+			}
+	if (top == 9)
+	{
+		_putchar('\n');
+	}
+	}
+}
+}
