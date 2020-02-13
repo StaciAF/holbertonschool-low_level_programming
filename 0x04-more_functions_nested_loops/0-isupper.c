ef6f5adf39db97651875bf36abf0b3b94e74f3e1
StaciAF <aaen.it19@gmail.com>
2020-02-13 16:49:37 +0000

    adds function that checks for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _isupper - checks for uppercase
+*
+*@c: character to be checked
+*
+*Description: checks for uppercase characters
+* Return: 1 for uppercase else 0
+*/
+
+int _isupper(int c)
+{
+	if (c == 65 || c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
