40448f6c8bdc68340c92402b881888c18af8c206
StaciAF <aaen.it19@gmail.com>
2020-02-13 18:41:51 +0000

    adds function that prints 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_numbers - prints 0 to 9
+*
+* Description: prints numbers 0 to 9 followed by new line"
+* Return: done in main
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 48;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+}
