c41599a5fc4b903e0b65f8dfe37391cca7e73f2f
StaciAF <aaen.it19@gmail.com>
2020-02-13 17:15:33 +0000

    adds function to check for 0 to 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* _isdigit - checks for 0 to 9
+*@c: digit to be checked
+*
+* Description: checks for 0 to 9
+* Return: 1 if single digit else 0
+*/
+
+int _isdigit(int c)
+{
+	if (c == 48 || c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
