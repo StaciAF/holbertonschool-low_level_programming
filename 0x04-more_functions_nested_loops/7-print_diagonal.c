3334d38fd2c42253fae31a01f2d6aa220a28677a
StaciAF <aaen.it19@gmail.com>
2020-02-14 01:34:25 +0000

    adds function that prints diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,14 +19,15 @@ void print_diagonal(int n)
 	else
 	{
 
-		for (i = 0; i <= n; i++)
-{
-		_putchar(92);
-		_putchar(10);
+		for (i = 0; i < n; i++)
+		{
 		for (j = 0; j < i; j++)
 		{
 		_putchar(32);
 		}
-	}
+		_putchar(92);
+		_putchar(10);
+
+		}
 	}
 }
