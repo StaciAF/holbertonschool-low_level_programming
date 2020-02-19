e4468a5aa98859774b1b174f794624b8d502c7a8
StaciAF <aaen.it19@gmail.com>
2020-02-19 23:57:05 +0000

    adds function to print given number of array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,13 +1,29 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
 * print_array - prints elements of an array
 * @a: pointer for integer array
 *
 * @n: integer
+*
 * Description: prints elements of array of integers
 * Return: in main
 */
 
 void print_array(int *a, int n)
 {
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		if (j != 0)
+		{
+		printf(",");
+		printf(" ");
+		}
+		printf("%d", a[j]);
+	}
+	putchar('\n');
+
+
 }
