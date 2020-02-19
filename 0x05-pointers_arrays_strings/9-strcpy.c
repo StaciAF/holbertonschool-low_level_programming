04af24ccee93e3f1089ee197b9b2293c3f4b6dbc
StaciAF <aaen.it19@gmail.com>
2020-02-19 00:43:54 +0000

    adds function that copies pointed string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _strcpy - copies a string pointed to by src
+ * @dest: pointer for buffer
+ *
+ * @src: pointer for string
+ * Description: copies a string pointed to by src
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
