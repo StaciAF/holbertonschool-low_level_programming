595ca4d543b98db766055a4ee64bfd010d313bc3
StaciAF <aaen.it19@gmail.com>
2020-02-19 00:38:27 +0000

    adds function that prints given elements in an intger array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* print_array - prints elements of an array
+* @a: pointer for integer array
+*
+* @n: integer
+* Description: prints elements of array of integers
+* Return: in main
+*/
+
+void print_array(int *a, int n)
+{
+
+}
