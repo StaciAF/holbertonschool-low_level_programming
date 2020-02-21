4dc2eb56eaf88d6ae0386dfd5881a049d304337f
StaciAF <aaen.it19@gmail.com>
2020-02-21 22:26:48 +0000

    adds function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,10 +4,23 @@
  *
  * @s: string given to change
  *
+ *
  * Description: encodes string into 1337
  * Return: nothing
  */
 
-char *leet(char *)
+char *leet(char *s)
 {
+
+	int i, j;
+      char letters[52] = "aAeEoOtTlL";
+      char num[12] = "4433007711";
+
+	for (i = 0; s[i]; i++)
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == letters[j])
+				s[i] = num[j];
+		}
+	return (s);
 }
