2303afc4ef95ec85f726827bc2ccfebaf9bcd07e
StaciAF <aaen.it19@gmail.com>
2020-02-21 02:01:14 +0000

    adds function that encodes using rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @s: string given to change
+ *
+ * Description: encodes string using rot13
+ * Return: nothing
+ */
+
+char *rot13(char *s)
+{
+}
