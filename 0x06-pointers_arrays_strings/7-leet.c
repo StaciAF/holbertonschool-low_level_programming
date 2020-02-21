9c14e5029592b7457d015c8ebf4fe22da1ac99d8
StaciAF <aaen.it19@gmail.com>
2020-02-21 01:57:51 +0000

    adds function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * leet - encodes string to 1337
+ *
+ * @s: string given to change
+ *
+ * Description: encodes string into 1337
+ * Return: nothing
+ */
+
+char *leet(char *)
+{
+}
