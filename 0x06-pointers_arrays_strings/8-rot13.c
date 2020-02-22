ad98bc6b0f52a12de2c43ed61a6c19ff1e1c31f5
StaciAF <aaen.it19@gmail.com>
2020-02-22 03:18:55 +0000

    adds function to encode with rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -5,9 +5,20 @@
  * @s: string given to change
  *
  * Description: encodes string using rot13
- * Return: nothing
+ * Return: string (s)
  */
 
 char *rot13(char *s)
 {
+	int i, j;
+char letters[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i]; i++)
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == letters[j])
+				s[i] = rot[j];
+		}
+	return (s);
 }
