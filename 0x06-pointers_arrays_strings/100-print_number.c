5c45176122f7b9c04aa6ecfd35151db1b41e05d6
StaciAF <aaen.it19@gmail.com>
2020-02-22 04:30:28 +0000

    adds function that prints an integer

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ *
+ * Description: prints an integer
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		_putchar(n + '0');
+
+	if (n == 0)
+		_putchar('0');
+
+	else
+		_putchar(n + '0');
+	}
+}
