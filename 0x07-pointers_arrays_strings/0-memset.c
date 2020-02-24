3e27a6d7b7efe2d00505b16761efea06549d6f84
StaciAF <aaen.it19@gmail.com>
2020-02-24 18:07:50 +0000

    adds function that mimics memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: memory area pointed to
+ *
+ * @b: constant byte
+ *
+ * @n: number of bytes
+ *
+ * Description: function that fills memory with a constant byte
+ * Return: s - pointer to the memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
