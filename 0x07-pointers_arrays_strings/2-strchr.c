efbcb0e4fe4d930d17052d9b517f979fe7865065
StaciAF <aaen.it19@gmail.com>
2020-02-24 19:35:43 +0000

    adds function that locates a char in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#define NULL 0
+/**
+ * _strchr - locates a char in a string
+ * @s: given string to be searched
+ *
+ * @c: char searched for
+ *
+ * Description: function that locates a character string; returns pointer
+ * Return: Null if not char found or return c (char)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (NULL);
+}
