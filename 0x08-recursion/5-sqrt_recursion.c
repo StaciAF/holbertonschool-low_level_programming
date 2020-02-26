c537f8b3a4a0756bee5a75f35a6e44e0e828c925
StaciAF <aaen.it19@gmail.com>
2020-02-26 23:31:05 +0000

    adds function that mimics sqrt recursviely

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - returns the natural sqrt
+ * @n: given number
+ *
+ * Description: function mimics sqrt command - recursively
+ *
+ * Return: sqrt or -1 if no natural sqrt
+ */
+int _sqrt_recursion(int n)
+{
+
+}
