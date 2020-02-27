885870ed4962e683295e8eb952e8654f459c8703
StaciAF <aaen.it19@gmail.com>
2020-02-27 23:50:19 +0000

    adds function that returns sqrt of given n

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,23 @@
+#include <stdio.h>
 #include "holberton.h"
+/**
+ * helper - finds input from n
+ * @bc: check against base case
+ *
+ * @n: given num to be squared
+ *
+ * Description: helper function for sqrt recursion
+ * Return: natural sqrt
+ */
+int helper(int n, int bc)
+{
+
+	if (n * n == bc)
+		return (n);
+	if (n * n > bc)
+		return (-1);
+	return (helper(n + 1, bc));
+}
 /**
  * _sqrt_recursion - returns the natural sqrt
  * @n: given number
@@ -9,5 +28,5 @@
  */
 int _sqrt_recursion(int n)
 {
-
+	return (helper(1, n));
 }
