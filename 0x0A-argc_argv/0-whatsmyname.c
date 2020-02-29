77f4008f5731e676026821d41fd2075969ff6223
StaciAF <aaen.it19@gmail.com>
2020-02-29 01:40:44 +0000

    adds program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ *
+ *
+ *
+ *
+ *
+ */
+int main (int argc __attribute__ ((unused)), char* argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
