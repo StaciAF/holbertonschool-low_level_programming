66ac5148b081c06be363edd347ffaed40acf68ee
StaciAF <aaen.it19@gmail.com>
2020-03-02 00:21:41 +0000

    adds program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
+/**
+ * is_num - determines if a string is a number
+ * @s: pointer to a string
+ *
+ * Return: 1 if number, 0 if not
+ */
 
+int is_num(char *s)
+{
+	while (*s != '\0')
+	{
+		if ((*s > '9') || (*s < '0'))
+			 return (0);
+		 s++;
+	}
+	 return (1);
+}
 /**
  * main - adds two numbers
  * @argc: argument counter
@@ -15,18 +32,20 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int counter;
 
-	if (argc < 1)
+	if (argc == 1)
 	{
 		printf("0\n");
+		return (0);
 	}
 	for (counter = 1; counter < argc; counter++)
 	{
-		if (!atoi(argv[counter]))
+		if (is_num(argv[counter]))
+			sum += atoi(argv[counter]);
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[counter]);
 	}
 	printf("%d\n", sum);
 	return (0);
