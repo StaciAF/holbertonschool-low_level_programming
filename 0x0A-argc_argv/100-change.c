dc5bfc3282991449ea075de31566a90c66b09892
StaciAF <aaen.it19@gmail.com>
2020-03-02 05:22:47 +0000

    adds program that calculates min number of coins to return

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - finds min num of coins as change
+ * @argv: array of pointers
+ *
+ * @argc: argument count
+ *
+ * Description: returns minimum number of coins
+ * Return: number of coins needed
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+	int change, i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (change < atoi(argv[i]))
+	{
+		printf("0\n");
+	}
+}
