a0a01097a8c7fbf7d0a37d94666f1f1e37b05542
StaciAF <aaen.it19@gmail.com>
2020-03-03 23:44:44 +0000

    adds function to concatenate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string taken to check length
+ *
+ * Description: returns the length of given string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
+/**
+ * str_concat - concatenates two strings
+ * @s1: first given string - in front of s2
+ *
+ * @s2: second given string - behind s1
+ *
+ * Description: concatenates two strings
+ * Return: NULL for failure otherwise return pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2;
+	int i, j;
+	char *ptr3;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	ptr3 = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ptr3[i] = s1[i];
+	}
+	for (j = 0; s2[j] != ' '; j++, i++)
+	{
+		ptr3[i] = s2[j];
+	}
+	ptr3[i] = '\0';
+	return(ptr3);
+
+}
