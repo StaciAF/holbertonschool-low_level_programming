22eb405cdff01abbad3f8609b6e9154e998a2c53
StaciAF <aaen.it19@gmail.com>
2020-03-03 20:00:41 +0000

    adds function that returns pointer with copy of string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _stardup - returns a pointer with malloc with a copy of given string
+ * @str: given string to be copied
+ *
+ * Description: returns a pointer with malloc with a copy of given string
+ * Return: NULL if str NULL or pointer containing copy of string
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int len = 0;
+	char *c;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (len = 0; str[len]; len++)
+	{
+		;
+	}
+	c = (malloc(len * (sizeof(*c))));
+
+	for (i = 0; i < len; i++)
+	{
+		c[i] = str[i];
+	}
+	return (c);
+}
