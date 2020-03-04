e7b1766001723f7a0cf0f7d15bf89d431d47e6f9
StaciAF <aaen.it19@gmail.com>
2020-03-04 04:21:04 +0000

    adds function that concatentes two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,6 +34,11 @@ char *str_concat(char *s1, char *s2)
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	ptr3 = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (ptr3 == NULL)
@@ -42,19 +47,10 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (s1 == 0)
-		{
-			s1 = "";
-		}
 		ptr3[i] = s1[i];
 	}
 	for (j = 0; s2[j]; j++, i++)
 	{
-		if (s2 == 0)
-		{
-			s2 = "";
-		}
-
 		ptr3[i] = s2[j];
 	}
 	ptr3[i] = '\0';
