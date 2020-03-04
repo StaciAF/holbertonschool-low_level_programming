9c7927c389af0935c0f619e1ec995c40a0bab9d5
StaciAF <aaen.it19@gmail.com>
2020-03-04 18:31:16 +0000

    adds function to concatenate two strings and return pointer

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,9 +35,13 @@ char *str_concat(char *s1, char *s2)
 	len2 = _strlen(s2);
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 	ptr3 = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (ptr3 == NULL)
