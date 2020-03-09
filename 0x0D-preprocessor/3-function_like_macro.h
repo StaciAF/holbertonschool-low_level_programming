a562d4b1c2dbf81f9b9bf0445cd05bd35a3b731c
StaciAF <aaen.it19@gmail.com>
2020-03-09 17:01:27 +0000

    adds header file with ABS(x) macro to compute abs val

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,2 +1,6 @@
-#include <stdio.h>
-#define ABS(x) ((x<0) ? (-x): (x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x < 0) ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
