94083d6d48827ed77457dd86e40612dfb1613f95
StaciAF <aaen.it19@gmail.com>
2020-03-09 20:53:28 +0000

    adds header file defining struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - details about a dog
+ * @name: dog's name
+ *
+ * @owner: owner's name
+ *
+ * @age: dog's age
+ *
+ * Description: Struct built for details about a dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
