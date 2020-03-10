b8cdee3ea92b62deb5f8302664e3ac8ac95138f6
StaciAF <aaen.it19@gmail.com>
2020-03-10 05:31:26 +0000

    adds function that creates new dog struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+/**
+ * new_dog - function creates new struct type dog
+ * @name: name of new dog
+ *
+ * @age: age of new dog
+ *
+ * @owner: name of new dog's owner
+ *
+ * Description: creates new dog struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog == NULL)
+	return (NULL);
+}
