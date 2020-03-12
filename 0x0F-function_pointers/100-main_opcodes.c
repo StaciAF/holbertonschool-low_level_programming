77e55306a7372ba50450817fd7f7d34ff393626f
StaciAF <aaen.it19@gmail.com>
2020-03-12 01:37:19 +0000

    adds program that prints opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * main - prints opcodes of its own main function
+ *
+ * @argc: argument counts
+ *
+ * @argv: argument values
+ *
+ */
+
+int main(int argc, char *argv)
+{
+	return (0);
+}
