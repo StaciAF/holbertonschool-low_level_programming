1693deecac07886a727806b873f16c8282264335
StaciAF <aaen.it19@gmail.com>
2020-03-12 03:43:26 +0000

    adds a function that prints an integer

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -12,12 +12,12 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		_putchar(n + '0');
+		_putchar(n);
+	}
 
 	if (n == 0)
 		_putchar('0');
 
 	else
-		_putchar(n + '0');
-	}
+		_putchar(n);
 }
