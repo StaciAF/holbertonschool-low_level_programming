862ce22c84b16256dd81c6131e673572894ffa8d
StaciAF <aaen.it19@gmail.com>
2020-03-12 04:01:57 +0000

    adds updated error message

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,10 +13,19 @@
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
+	int bytes;
+
+	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	bytes = atoi(argv[1]);
+		     if (bytes < 0)
+		     {
+			     printf("Error\n");
+				     exit(2);
+		     }
+
 	return (0);
 }
