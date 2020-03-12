cea3e9f89e2156bf124a1daa5a747b5de47bfb6d
StaciAF <aaen.it19@gmail.com>
2020-03-12 03:28:32 +0000

    added break statement to correct output

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * rot13 - encodes a string using rot13
  *
@@ -11,14 +12,19 @@
 char *rot13(char *s)
 {
 	int i, j;
-char letters[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char rot[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+char letters[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char rot[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i]; i++)
-		for (j = 0; j < 52; j++)
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 53; j++)
 		{
 			if (s[i] == letters[j])
+			{
 				s[i] = rot[j];
+				break;
+			}
 		}
+	}
 	return (s);
 }
