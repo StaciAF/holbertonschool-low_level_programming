9ca43926b499d9a6282898c18664655e03aaf428
StaciAF <aaen.it19@gmail.com>
2020-03-13 17:45:07 +0000

    adds helper functions for operators

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - adds two integers
+ * @a: first integer input
+ *
+ * @b: second integer input
+ *
+ * Description: adds two integers
+ * Return: Nothing
+ */
+int op_add(int a, int b)
+{
+	(a + b);
+}
+/**
+ * op_sub - subtracts two integers
+ * @a: first integer input
+ *
+ * @b: second integer input
+ *
+ * Description: subs two integers
+ * Return: Nothing
+ */
+
+int op_sub(int a, int b)
+{
+	(a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: first integer input
+ *
+ * @b: second integer input
+ *
+ * Description: multiplies two integers
+ * Return: Nothing
+ */
+
+int op_mul(int a, int b)
+{
+	(a * b);
+}
+/**
+ * op_div - divides two integers
+ * @a: first integer input
+ *
+ * @b: second integer input
+ *
+ * Description: divides two integers
+ * Return: Nothing
+ */
+
+int op_div(int a, int b)
+{
+	(a / b);
+}
+/**
+ * op_mod - modulo two integers
+ * @a: first integer input
+ *
+ * @b: second integer input
+ *
+ * Description: mods two integers
+ * Return: Nothing
+ */
+
+int op_mod(int a, int b)
+{
+	(a % b);
+}
