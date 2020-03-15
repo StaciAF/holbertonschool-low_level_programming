71938de3d93d502313f664c25bd1e1d9b11f414d
StaciAF <aaen.it19@gmail.com>
2020-03-15 02:23:34 +0000

    adds function that prints numbers passed with variable args

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints integers passed to function
+ * @separator: the string to be printed between numbers
+ *
+ * @n: number of integers passed
+ *
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, unsigned int));
+		if (i != (n - 1) && separator != 0)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+
+	va_end(args);
+}
