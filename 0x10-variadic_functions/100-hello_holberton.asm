ae2f513b37d2f2f8a86386bed44febc2a8c6c55a
StaciAF <aaen.it19@gmail.com>
2020-03-16 05:50:43 +0000

    adds 64-bit program in assembly that prints

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,12 @@
-section .data
+.cstring
+_hello:	 .asciz "Hello, Holberton\n"
 
-	    message db "Hello, Holberton\n", 10
-
-	section .text
-
-	global _start
-_start:
-	    mov rax, 1
-	    mov rdi, 1
-	    mov rsi, message
-	    mov rdx, 14
-	    syscall
+	.text
+	.globl _main
+_main:
+	    sub  $8, %rsp
+	    mov  $0, %rax
+	    lea  _hello(%rip), %rdi
+	    call _printf
+	    add  $8, %rsp
+	    ret
