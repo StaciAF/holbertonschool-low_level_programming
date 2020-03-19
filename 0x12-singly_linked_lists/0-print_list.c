33d32799ebe2e5fbbdbe9bc3ac37a906a136a932
StaciAF <aaen.it19@gmail.com>
2020-03-19 21:08:10 +0000

    adds function that prints elements of list_t struct

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: points to list - head of list to be counted
+ *
+ * return: number of nodes in list
+ */
+
+size_t print_list(const list_t *h)
+{
+	int nodes;
+
+	nodes = 0;
+	while (h != '\0')
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
