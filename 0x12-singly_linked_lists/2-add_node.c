0038f84a7a9af717bd75ef0daf978cadbc9b909a
StaciAF <aaen.it19@gmail.com>
2020-03-19 23:35:18 +0000

    trying to fix edge case checks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <string.h>
 /**
  * add_node - adds a new node at beginning of list
  * @head: pointer to pointer of list
@@ -18,7 +18,6 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new == NULL)
 		return (NULL);
-
 	new->len = strlen(str);
 	new->str = strdup(str);
 	new->next = *head;
