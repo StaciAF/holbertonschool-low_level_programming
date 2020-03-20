752a97bbb8bbd87cdf315cb5e5a272a7565ae7f0
StaciAF <aaen.it19@gmail.com>
2020-03-20 01:46:54 +0000

    adds function to free malloc'd list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: list to be freed of memory
+ *
+ * Description: frees malloced list space
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
