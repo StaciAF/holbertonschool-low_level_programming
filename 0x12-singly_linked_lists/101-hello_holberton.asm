81fe24552304d4928f0787e4877dcf55f0e79707
StaciAF <aaen.it19@gmail.com>
2020-03-20 02:12:28 +0000

    adds 64bit program in assembly language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+;  The purpose of this program is to print Hello, Holberton
+
+segment .data
+	 hello db "Hello, Holberton", 0xa, 0
+
+	segment .text
+	 extern printf
+	 extern exit
+
+	global main
+main:
+     mov rax, 0
+     mov rdi, hello
+     call printf
+
+     mov rax, 0
+     mov rdi, 0
+     call exit
