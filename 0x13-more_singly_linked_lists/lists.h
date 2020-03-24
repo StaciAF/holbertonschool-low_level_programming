9ab4478ad3fe04da93409d4937ed16cf627d90f7
StaciAF <aaen.it19@gmail.com>
2020-03-24 15:39:27 +0000

    adds updated header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,6 +9,22 @@
 #include <stddef.h>
 
 /**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
@@ -20,7 +36,8 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint_t;
+}
+listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
