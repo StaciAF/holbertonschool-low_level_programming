cd734aff0c52048f44e2655c85f9e0a5b7647911
StaciAF <aaen.it19@gmail.com>
2020-03-24 17:19:20 +0000

    adds function that frees double pointer list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a linked list
+ * @head: double pointed to link list; to be freed
+ *
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while(*head != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+	*head = NULL;
+}
