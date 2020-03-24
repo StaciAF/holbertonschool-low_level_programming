668a0fc14decb075ff474e690885d944173c83bb
StaciAF <aaen.it19@gmail.com>
2020-03-24 22:52:48 +0000

    rearranged syntax

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,13 +16,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	len = 0;
 	temp = head;
 
-	if (head == NULL)
-		return (NULL);
 
 	while (temp != NULL)
 	{
 		len++;
 		temp = temp->next;
+		if (head == NULL)
+			return (NULL);
 
 		if (len == index)
 		{
@@ -34,7 +34,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		for (i = 1; i < len - index + 1; i++)
 		{
 			temp = temp->next;
-			return (head);
 		}
-		return (NULL);
+			return (temp);
 }
