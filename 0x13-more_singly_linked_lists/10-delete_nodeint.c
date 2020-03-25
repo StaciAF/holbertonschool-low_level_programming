bb2261a25319c9d9ec34ea8de06b3cd68b409eb5
StaciAF <aaen.it19@gmail.com>
2020-03-25 17:41:35 +0000

    added null check for head

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *prev, *temp;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 
 	while (i != index && *head != NULL)
