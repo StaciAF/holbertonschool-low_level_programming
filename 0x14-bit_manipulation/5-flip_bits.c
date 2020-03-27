ceea3f781d74d1ca505bf6c77ec0934d59fa0411
StaciAF <aaen.it19@gmail.com>
2020-03-27 03:11:47 +0000

    adds function to count bit flips to convert integers

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - counts how many bits flipped to get to new number
+ * @n: first number to be compared
+ *
+ * @m: second number to be compared
+ *
+ * Return: count of flipped bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int flips = 0;
+	unsigned int z;
+
+	for (z = n ^ m; z != 0; z = z >> 1)
+		flips = flips + (z & 1);
+
+	return (flips);
+}
