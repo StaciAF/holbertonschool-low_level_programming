35203c5751f93f9e0b3faaa63c34e3e8c058e235
StaciAF <aaen.it19@gmail.com>
2020-03-27 04:36:50 +0000

    updated with _putchar not printf

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * print_binary - prints the binary representation of a number
@@ -15,12 +28,12 @@ void print_binary(unsigned long int n)
 
 	if (num == 1)
 	{
-		printf("1");
+		_putchar('1');
 		return;
 	}
 	if (num == 0)
 	{
-		printf("0");
+		_putchar('0');
 		return;
 	}
 
@@ -29,10 +42,10 @@ void print_binary(unsigned long int n)
 		result = n & (1 << cnt);
 		if (result)
 		{
-			printf("1");
+			_putchar('1');
 		}
 		else
-			printf("0");
+			_putchar('0');
 	}
 
 }
