02a5e87c90fcf4611bf181cef1ef7c29cbe7440a
StaciAF <aaen.it19@gmail.com>
2020-04-01 00:19:35 +0000

    adds updated permission calls

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		thisfile = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+       thisfile = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 		return (1);
 	}
 
-	thisfile = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+       thisfile = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (thisfile == -1)
 		return (-1);
