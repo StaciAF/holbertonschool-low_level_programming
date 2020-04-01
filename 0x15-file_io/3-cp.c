20dd5d1ba8a1cae07cc3424b85e7c437c9de914b
StaciAF <aaen.it19@gmail.com>
2020-04-01 04:42:44 +0000

    adds program that copies one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,11 +10,25 @@
 
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
+	char *file_from = argv[1];
+	char *file_to = argv[2];
 
 	if (argc != 3)
 	{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
+	}
+
+	if (file_from == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
+	}
+
+	if (file_to == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+			exit(99);
 	}
 	return (0);
 }
