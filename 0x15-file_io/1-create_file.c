2d566403affcfc951086f639dff5079ad00999c6
StaciAF <aaen.it19@gmail.com>
2020-04-01 00:45:09 +0000

    removed buffer unused, cleaned up syntax

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,28 +11,25 @@
 int create_file(const char *filename, char *text_content)
 {
 	int thisfile, nocontent;
-	char *buffer;
 
 	if (filename == NULL)
+		return (-1);
+
+thisfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (thisfile == -1)
 		return (-1);
 
 	if (text_content == NULL)
 		return (1);
 
-thisfile = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 nocontent = write(thisfile, text_content, _strlen(text_content));
+
+close(thisfile);
 
 if (nocontent == -1)
 	return (-1);
 
-	if (thisfile == -1)
-		return (-1);
-
-	buffer = malloc(sizeof(*text_content));
-
-	write(STDOUT_FILENO, buffer, _strlen(buffer));
-	free(buffer);
-	close(thisfile);
 	return (1);
 }
 /**
