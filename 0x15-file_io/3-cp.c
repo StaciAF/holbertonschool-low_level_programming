4d514de115352dd65a76541902f122a0fea0f0d9
StaciAF <aaen.it19@gmail.com>
2020-04-05 19:02:22 +0000

    adds program that copies contents of one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,28 +7,42 @@
  *
  * Return: Nothing.
  */
-
 int main(int argc, char *argv[])
+{       int op_to, op_from, rd, wr;
+	char buff[1024];
+
+if (argc != 3)
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+exit(97);
+	op_from = open(argv[1], O_RDONLY);
+if (op_from == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+exit(98);
+	op_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+if (op_to == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+exit(99);
+	rd = read(op_from, buff, 1024);
+	while (rd > 0)
+	{
+		wr = write(op_to, buff, rd);
+if (wr == -1)
 {
-	char *file_from = argv[1];
-	char *file_to = argv[2];
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
-			exit(97);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+exit(99);
+}
+		rd = read(op_from, buff, 1024);
 	}
-
-	if (file_from == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-			exit(98);
-	}
-
-	if (file_to == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
-			exit(99);
-	}
+if (rd == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+exit(98);
+op_from = close(op_from);
+op_to = close(op_to);
+if (op_from == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op_from),
+exit(100);
+if (op_to == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op_to),
+exit(100);
 	return (0);
 }
