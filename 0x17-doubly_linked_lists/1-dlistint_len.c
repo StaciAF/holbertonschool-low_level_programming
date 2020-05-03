6e212ee7992c21c67f827db0e333c0b3b0d51df1
StaciAF <aaen.it19@gmail.com>
2020-05-03 17:57:20 +0000

    adding everything, debugging

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * dlistint_len - returns the number of elements in doubly linked list
  * @h: pointer to head node of doubly linked list
  *
- * Returns: number of elements in linked list
+ * Return: number of elements in linked list
  */
 
 size_t dlistint_len(const dlistint_t *h)
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,22 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string taken to check length
- *
- * Description: returns the length of given string
- * Return: length of string
- */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c] != '\0')
-		c++;
-	return (c);
-}
-
-/**
  * add_dnodeint - adds new node at beginning of doubly linked list
  * @head: pointer to head node of linked list
  *
