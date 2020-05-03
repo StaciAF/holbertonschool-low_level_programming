d11c28ce66d4379f6b1ff8c293b4859bc63b9411
StaciAF <aaen.it19@gmail.com>
2020-05-03 18:47:29 +0000

    adds function to return value of given node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dnodeint_at_index - returns the indexed node in a doubly linked list
+ * @head: pointer to head of doubly linked list
+ *
+ * @index: given node index to be returned
+ *
+ * Return: value of indexed node or NULL
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int len;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (len = 0; len < index; len++)
+	{
+		head = head->next;
+	}
+	return (head);
+}
