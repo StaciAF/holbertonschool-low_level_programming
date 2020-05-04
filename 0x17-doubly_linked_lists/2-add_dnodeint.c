8a6ff74080ea594291ed1703249c219d47c16cfe
StaciAF <aaen.it19@gmail.com>
2020-05-04 15:26:11 +0000

    added check for head NULL at beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,11 +12,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode;
 
+	if (head == NULL)
+		return (NULL);
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
 	newnode->next = *head;
+	newnode->prev = NULL;
 	*head = newnode;
 	return (newnode);
 }
