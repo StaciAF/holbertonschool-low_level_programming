e8e9ee8f61e45897296435d01790441da866ea90
StaciAF <aaen.it19@gmail.com>
2020-06-04 15:42:49 +0000

    pushing all files, removed index call in set, corrected delete

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,9 +13,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	const unsigned char *new_key = (const unsigned char *) key;
 	unsigned long int i;
 
-	i = hash_djb2(new_key) % ht->size;
 	if (ht == NULL)
 		return (0);
+	i = hash_djb2(new_key) % ht->size;
 	if (key == NULL || (strcmp(key, "") == 0) || value == NULL)
 		return (0);
 	if (ht->array[i] == NULL)
@@ -24,17 +24,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (0);
 		ht->array[i] = new_el;
 		new_el->key = strdup(key);
-		new_el->value = strdup(value);
-		new_el->next = NULL;
+		new_el->value = strdup(value), new_el->next = NULL;
 		return (1);
 	}
 	element = ht->array[i];
-	while (element != NULL)
+	while (element)
 	{
 		if (strcmp(element->key, key) == 0)
 		{
 			free(element->value);
-			free(element);
 			element->value = strdup(value);
 			return (1);
 		}
@@ -48,8 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[i] = new_el;
 		new_el->key = strdup(key);
 		new_el->value = strdup(value);
-		new_el->next = element;
-		ht->array[i] = new_el;
+		new_el->next = element, ht->array[i] = new_el;
 		return (1);
 	}
 	return (0);
