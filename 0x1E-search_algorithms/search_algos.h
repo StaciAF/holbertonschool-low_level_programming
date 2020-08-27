bbb4e6233d2f78646864e02acab4a32b0d62e0a6
StaciAF <aaen.it19@gmail.com>
2020-08-27 17:18:29 +0000

    adds header file for project

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/** TASK 0 */
+int linear_search(int *array, size_t size, int value);
+
+/** TASK 1 */
+int binary_search(int *array, size_t size, int value);
+
+#endif
